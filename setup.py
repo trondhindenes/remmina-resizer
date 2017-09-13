bb35a05331653f6ae97edb66ce19fef3a7d1eb1b
Trond Hindenes <trond@hindenes.com>
2017-09-13 23:59:57 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='remmina_resizer',
-    version='0.0.3',
+    name='remmina-resizer',
+    version='0.0.4',
     packages=['remmina_resizer'],
     description='A simple package for auto-adjusting screen resolutionsin Remmina connection files',
     author='Trond Hindenes',
     author_email='trond@hindenes.com',
     url='https://github.com/trondhindenes/remmina-resizer',
-    download_url='https://github.com/trondhindenes/remmina-resizer/archive/0.0.3.tar.gz',
+    download_url='https://github.com/trondhindenes/remmina-resizer/archive/0.0.4.tar.gz',
     install_requires=[
         'screeninfo',
         'ruamel.yaml',
