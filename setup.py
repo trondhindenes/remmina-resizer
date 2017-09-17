98eacf7f9437d7d2865931c3d724e0043fc27b76
Trond Hindenes <trond@hindenes.com>
2017-09-17 22:33:45 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='remmina-resizer',
-    version='0.0.4',
+    version='0.0.5',
     packages=['remmina_resizer'],
     description='A simple package for auto-adjusting rdp session resolution in Remmina connection files',
     long_description="Remmina (http://www.remmina.org) is a great RDP tool, but it has some limitations regarding screen resolution, which this project attempts to solve. See the readme at https://github.com/trondhindenes/remmina-resizer/blob/master/README.md for full info",
     author='Trond Hindenes',
     author_email='trond@hindenes.com',
     url='https://github.com/trondhindenes/remmina-resizer',
-    download_url='https://github.com/trondhindenes/remmina-resizer/archive/0.0.4.tar.gz',
+    download_url='https://github.com/trondhindenes/remmina-resizer/archive/0.0.5.tar.gz',
     install_requires=[
         'screeninfo',
         'ruamel.yaml',
